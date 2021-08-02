c215f35107b05bb27149bfc106feeefaab4b33d5
BarshaC <barsha.chaudhary@bison.howard.edu>
2021-08-02 19:20:39 -0400

    made some changes to a couple of files. need to fix the ackerman function though

--- chapter6/Exercises/sixpoint5.py
+++ chapter6/Exercises/sixpoint5.py
@@ -1,10 +1,14 @@
-def ack(m,n):
-    if m==0:
-        return n+1 
-    elif m>0 and n==0:
-        m= m-1
-        n=1 
-        return 
-    if m>0 and n >0:
-        return ack(m-1,ack(m,n-1))
-ack(3,4)
+"""
+implementing ackerman function recursively.
+need to fix this fuction
+"""
+##def ack(m,n):
+##    if m==0:
+##        return n+1 
+##    elif m>0 and n==0:
+##        m= m-1
+##        n=1 
+##        return ack(m-1,1)
+##    if m>0 and n>0:
+##        return ack(m-1,ack(m,n-1))
+##ack(3,4)
